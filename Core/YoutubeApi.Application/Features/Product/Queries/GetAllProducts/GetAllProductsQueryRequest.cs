598cc7ee0405aee0fa760c0ef543ca8d5a918f46
tair800 <146845940+tair800@users.noreply.github.com>
2025-02-09 08:56:46 +0100

    autoMapper

---
+++ Core/YoutubeApi.Application/Features/Product/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace YoutubeApi.Application.Features.Product.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryResponse>>
+    {
+    }
+}
