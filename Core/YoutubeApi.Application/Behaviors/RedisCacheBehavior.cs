da274761f90494ad0751de4b13843822257b60d9
tair800 <146845940+tair800@users.noreply.github.com>
2025-02-25 08:18:54 +0100

    Redis connection,service,behavior

---
+++ Core/YoutubeApi.Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using YoutubeApi.Application.Interfaces.RedisCache;
+
+namespace YoutubeApi.Application.Behaviors
+{
+    public class RedisCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query)
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+
+                var cacheData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if (cacheData is not null)
+                    return cacheData;
+
+                var response = await next();
+                if (response is not null)
+                    await redisCacheService.SetASync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ Core/YoutubeApi.Application/Interfaces/RedisCache/IRedisCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace YoutubeApi.Application.Interfaces.RedisCache
+{
+    public interface IRedisCacheService
+    {
+        Task<T> GetAsync<T>(string key);
+        Task SetASync<T>(string key, T value, DateTime? expirationTime = null);
+    }
+}
